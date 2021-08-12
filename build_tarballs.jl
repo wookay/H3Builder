20284ca72acc976a4882778ea5cf249c3bf0c6f9
WooKyoung Noh <wookay.noh@gmail.com>
2021-08-12 17:06:46 +0900

    h3 3.7.2

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 name = "H3"
-version = v"3.6.3"
+version = v"3.7.2"
 
 #=
 using BinaryProvider
@@ -10,21 +10,21 @@ function url2hash(url)
      path = download(url)
      open(io-> bytes2hex(BinaryProvider.sha256(io)), path)
 end
-url2hash("https://github.com/uber/h3/archive/v3.6.3.zip") |> println
+url2hash("https://github.com/uber/h3/archive/v3.7.2.zip") |> println
 =#
 
 sources = [
-    "https://github.com/uber/h3/archive/v$version.zip" => "33cea2c77961a7736c81f2cd7f23131aa1ff71ce68bb5b3facca40a257e0741f",
+    "https://github.com/uber/h3/archive/v$version.zip" => "805f787912608b09afa7c0e9c432c0e4b43f17a9d10faed880b3f74f038ce576",
 ]
 
 # Bash recipe for building across all platforms
 script = """
 cd \$WORKSPACE/srcdir/h3-$version/
 cat <<EOF > CMakeLists.txt.patch
-diff -uNr h3-3.6.3-original/CMakeLists.txt h3-3.6.3/CMakeLists.txt
---- h3-3.6.3-original/CMakeLists.txt	2019-12-15 20:20:53.000000000 +0900
-+++ h3-3.6.3/CMakeLists.txt	2019-12-15 20:21:42.000000000 +0900
-@@ -56,6 +56,9 @@
+diff -uNr h3-3.7.2-original/CMakeLists.txt h3-3.7.2/CMakeLists.txt > CMakeLists.txt.patch
+--- h3-3.7.2-original/CMakeLists.txt    2021-08-12 16:32:04.000000000 +0900
++++ h3-3.7.2/CMakeLists.txt 2021-08-12 16:33:03.000000000 +0900
+@@ -58,6 +58,9 @@
 
  project(h3 LANGUAGES C VERSION \\\${H3_VERSION})
 
@@ -34,33 +34,11 @@ diff -uNr h3-3.6.3-original/CMakeLists.txt h3-3.6.3/CMakeLists.txt
  set(H3_COMPILE_FLAGS "")
  set(H3_LINK_FLAGS "")
  if(NOT WIN32)
-@@ -537,21 +539,6 @@
- endif()
-
- if(BUILD_BENCHMARKS)
--    # Benchmarks
--    add_custom_target(benchmarks)
--
--    macro(add_h3_benchmark name srcfile)
--        add_h3_executable(\\\${name} \\\${srcfile} \\\${APP_SOURCE_FILES})
--        add_custom_target(bench_\\\${name} COMMAND \\\${TEST_WRAPPER} \\\$<TARGET_FILE:\\\${name}>)
--        add_dependencies(benchmarks bench_\\\${name})
--    endmacro()
--
--    add_h3_benchmark(benchmarkH3Api src/apps/benchmarks/benchmarkH3Api.c)
--    add_h3_benchmark(benchmarkKRing src/apps/benchmarks/benchmarkKRing.c)
--    add_h3_benchmark(benchmarkH3Line src/apps/benchmarks/benchmarkH3Line.c)
--    add_h3_benchmark(benchmarkH3SetToLinkedGeo src/apps/benchmarks/benchmarkH3SetToLinkedGeo.c)
--    add_h3_benchmark(benchmarkPolyfill src/apps/benchmarks/benchmarkPolyfill.c)
--    add_h3_benchmark(benchmarkPolygon src/apps/benchmarks/benchmarkPolygon.c)
- endif()
-
- # Installation (https://github.com/forexample/package-example)
 EOF
 patch -p1 -i CMakeLists.txt.patch
 mkdir build
 cd build
-cmake -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX=\${prefix} -DCMAKE_TOOLCHAIN_FILE=\${CMAKE_TARGET_TOOLCHAIN} ..
+cmake -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX=\${prefix} -DCMAKE_TOOLCHAIN_FILE=\${CMAKE_TARGET_TOOLCHAIN} -DBUILD_BENCHMARKS=0 -DBUILD_TESTING=0 ..
 make
 make install
 rm -rf \$WORKSPACE/destdir/bin/{g,h,k}* \$WORKSPACE/destdir/lib/cmake \$WORKSPACE/destdir/logs
